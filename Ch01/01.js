55d665bdfed42e1d2402f0e40c6465241b9ff36d
GamerzUnite <spdpvd3515@gmail.com>
2024-05-27 18:56:35 +0530

    Start of learning

---
+++ Ch01/01.js
@@ -0,0 +1,28 @@
+console.log("Hello World First Js");
+
+a = 1;
+b = 2.1;
+c = "My name is Soham";
+d = null;
+e = undefined;
+follow = true;
+unfollow = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(follow);
+console.log(unfollow);
+
+
+{
+    let a = 24;
+    console.log(a);
+    console.log("Both are printing A")
+}
+{
+    let a = 22; console.log(a);
+    console.log("Both are printing A")
+}
