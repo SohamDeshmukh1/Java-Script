9afc1318c3a9ec5380721f5ee8bfccbabb4ddffb
GamerzUnite <spdpvd3515@gmail.com>
2024-05-28 13:30:52 +0530

    Pretty basic

---
+++ Ch02/script.js
@@ -0,0 +1,23 @@
+console.log("Just some Basic Arithamatic Operatios");
+a = 10;
+b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log("For The use of ^ for any number use Variable_name**");
+b = b**2;
+console.log("5^2 is : ",b);
+
+//Method to print variable in between
+console.log("Value of a is: ",a,"Value of be is ",b);
+
+console.log("For increment decrement ++ and --");
+
+// we know the basics such as += , -= , *= , /= ,%= 
+// and for comparision >,<,<=,>=,==,!=,===,!==
+// logical operators &&,||,!
+// This are all the mostly needed operators Basics
