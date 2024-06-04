db174589938a4fe2b3a81b2c5ed4f0d9d3a77e2c
GamerzUnite <spdpvd3515@gmail.com>
2024-06-04 22:18:04 +0530

    for each

---
+++ Ch05/05.1.js
@@ -0,0 +1,15 @@
+// New learning for each loop
+// Total New loops 1] For of,2] For in, 3]For each
+// Example
+
+let arr = [1,2,3,4,5];
+arr.forEach((val)=>{
+    console.log(val);
+}
+)
+
+
+arr.forEach((val)=>{
+    console.log(`Square of ${val} value is ${val**2}`);
+}
+)
