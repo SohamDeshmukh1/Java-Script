5af7eac2f2dd56592f45a9fdeaf606b49b5ce876
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-06 15:14:11 +0530

    DOM Set Attribute Methods

---
+++ CH07/1.js
@@ -0,0 +1,5 @@
+let div1 = document.querySelector(".box");
+console.log(div1.setAttribute("class","editedClass1"));
+
+let div2 = document.querySelector(".box1");
+console.log(div2.setAttribute("class","editedClass2"));
