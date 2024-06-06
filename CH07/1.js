3951c6a68ae24b1eca19afe6bbeaed0ee81731d3
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-06 16:41:05 +0530

    DOM Manipulation

--- CH07/1.js
+++ CH07/1.js
@@ -3,3 +3,19 @@ console.log(div1.setAttribute("class","editedClass1"));
 
 let div2 = document.querySelector(".box1");
 console.log(div2.setAttribute("class","editedClass2"));
+
+div2.style.backgroundColor = "white";
+div1.style.color = 'black';//node name.style.attribute (we can acces and put css through it too);\
+div2.style.color = 'black';
+
+let btn1 = document.createElement("button");
+btn1.textContent = "1 HEHEHHE";
+
+let newbtn = document.createElement("button");//Creating a btn
+newbtn.innerText = "Submit Form";
+div2.append(newbtn);//inserting it at the end of the div
+
+div2.prepend(btn1);
+
+//prepend and append inserts inside a node of dom structure 
+//before and after we can put the element before or after any node if we want
