65960604a7aee3271b4e86284774e43de16c7dc0
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-09 09:44:44 +0530

    Ch Completed

--- CH10/script.js
+++ CH10/script.js
@@ -1,6 +1,6 @@
 let cho = document.querySelectorAll(".choice");
-let comp = document.querySelector('.compScore');
-let user = document.querySelector('.user');
+let comp = document.querySelector('#compScore');
+let user = document.querySelector('#user');
 let txt = document.querySelector('#txt1');
 
 let userScore1 = 0;
@@ -15,29 +15,29 @@ function randstrig() {
 let playgame = (userchoice) => {
     let computer = randstrig();
     if (userchoice === computer) {
-        txt.textContent = "It's a tie!"
-        }else {
-            let userwin = false;
-            if (userchoice === 'rock') {
-                userwin = computer === 'scissors';
-                txt.textContent = userwin ? "You win! Rock beats Scissors" : "You lose! Paper beats Rock";
-            } else if (userchoice === 'paper') {
-                userwin = computer === 'rock';
-                txt.textContent = userwin ? "You win! Paper beats Rock" : "You lose! Scissors beats Paper";
-            } else if (userchoice === 'scissors') {
-                userwin = computer === 'paper';
-                txt.textContent = userwin ? "You win! Scissors beats Paper" : "You lose! Rock beats Scissors";
-            }
-    
-            if (userwin) {
-                userScore1++;
-            } else {
-                compScore1++;
-            }
-            comp.textContent = compScore1;
-            user.textContent = userScore1;
+        txt.textContent = "It's a tie!";
+    } else {
+        let userwin = false;
+        if (userchoice === 'rock') {
+            userwin = computer === 'scissors';
+            txt.textContent = userwin ? "You win! Rock beats Scissors" : "You lose! Paper beats Rock";
+        } else if (userchoice === 'paper') {
+            userwin = computer === 'rock';
+            txt.textContent = userwin ? "You win! Paper beats Rock" : "You lose! Scissors beats Paper";
+        } else if (userchoice === 'scissors') {
+            userwin = computer === 'paper';
+            txt.textContent = userwin ? "You win! Scissors beats Paper" : "You lose! Rock beats Scissors";
         }
+
+        if (userwin) {
+            userScore1++;
+        } else {
+            compScore1++;
+        }
+        comp.textContent = compScore1;
+        user.textContent = userScore1;
     }
+}
 
 cho.forEach((choice) => {
     choice.addEventListener("click", () => {
@@ -45,9 +45,3 @@ cho.forEach((choice) => {
         playgame(userchoice);
     });
 });
-
-    
-
-    
-
-
