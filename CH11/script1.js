ac9ee1eac2fee5c5e2fa815551dd51514c22c084
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-13 11:22:42 +0530

    Single

---
+++ CH11/script1.js
@@ -0,0 +1,22 @@
+// all about inheritance
+
+class carsget{
+    constructor(brand,model,year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+}
+class carsShow extends carsget{
+    getCarInfo(){
+        console.log( `The car is a ${this.year} ${this.brand} ${this.model}`);
+    }
+}
+
+let sup1 = new carsShow('Audi','s100',2024);
+// sup1.brand = "Toyota";
+// sup1.model = "Camry";
+// sup1.year = 2015; Both method works
+sup1.getCarInfo();
+// Single Inheritence
