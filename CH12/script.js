347d4bf6d3ba317d7a98db219b15f9ba0c1eb241
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-14 15:16:19 +0530

    Update script.js

--- CH12/script.js
+++ CH12/script.js
@@ -15,7 +15,7 @@ function say(hello){
     setTimeout(()=>{
         console.log(`I Am Soham Deshmukh`);
         hello();
-    },3000);
+    },1000);
 }
 
 function greet(){
@@ -36,4 +36,17 @@ setTimeout(()=>{
         console.log(`Subtraction is ${a-b}`)
     }
     calculator(5,2,add,sub);
-},50000); // This will all process will get execute in 5 sec till then all the code above gets executed
+},1000); // This will all process will get execute in 5 sec till then all the code above gets executed
+
+// Some timeout + callback example 
+function data(getdata,nextdata){
+    setTimeout(()=>{
+        console.log(`the data is ${getdata}`);
+        nextdata();
+    },2000);//delay of 2000 ms = 2 sec;
+}
+data(1 , ()=>
+{
+    data(2); // in this i gave value to nextdata in data so the data will fetch value from here
+    // some small issue
+})
