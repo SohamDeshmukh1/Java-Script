ac67f3267b3fb6740fe876eedeb54bc56e3b17db
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-17 10:08:56 +0530

    Async Function Practice

--- CH12/3.js
+++ CH12/3.js
@@ -1,5 +1,4 @@
 // promise example
-
 const pro = ()=>{
     return new Promise((resolve,reject)=>{
         console.log(`This is a promise i am making`);
@@ -7,12 +6,29 @@ const pro = ()=>{
         reject(`Invalid Data Output`);
     });
 }
-
 let n1 = pro();
 n1.then((res)=>{
     console.log(`promise resolved`,res)
 });
-
 n1.catch((err)=>{
 console.log(`Rejected, `,err);
 })
+
+// Async function example
+// This function can work in anyorder due to having an settimeout they dont work as 
+// sync function which gets exectued 1 by 1
+
+function Asyn(){
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        console.log('Data 1');
+        resolve(`Sucess`);
+       },3000);
+    });
+};
+console.log(`fetching data`);
+let new1 = Asyn();
+new1.then((res)=>
+{
+    console.log(res);
+});
