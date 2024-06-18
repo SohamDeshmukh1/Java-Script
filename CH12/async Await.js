ce62720f03b71cf3f302cc09b0fedf0fd4774c22
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-18 14:40:24 +0530

    Update async Await.js

--- CH12/async Await.js
+++ CH12/async Await.js
@@ -3,12 +3,14 @@
 function api(){
     return new Promise((resolve,reject)=>{
         setTimeout(() => {
-            console.log(`Weather Sunny`);
+            console.log('Weather is Sunny');
             resolve(200);
         }, 2000);
     });
 };
 
 async function weather(){
+    console.log(`Fetching API`)
     await api();
+    console.log(`API fetched`)
 }
