e137fa3d6e8688ecb076b3944311e675f6fefa28
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-18 14:49:16 +0530

    Callback hell but with Async

--- CH12/async Await.js
+++ CH12/async Await.js
@@ -14,3 +14,22 @@ async function weather(){
     await api();
     console.log(`API fetched`)
 }
+
+// Now using async for the previous callback hel example 
+
+function data(getdata){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log(`The data is ${getdata}`);
+            resolve(200);
+        }, 2000);
+    })
+}
+async function get1 (){
+    await data(1);
+    await data(2);
+    await data(3);
+    await data(4);
+    await data(5);
+    await data(6);
+};
