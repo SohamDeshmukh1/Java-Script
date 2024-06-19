48479a87f89c99a72a1f0b96d849b5a198d15438
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-19 12:02:05 +0530

    Some confusing stuff

--- CH13/script.js
+++ CH13/script.js
@@ -1,10 +1,27 @@
-// Dont mind the project ;-;
+// Dont mind the small project;
 
-const url ='https://api.waifu.im/search';
+const url ='https://cat-fact.herokuapp.com/facts';
 
+let p1 = document.querySelector('#fact');
+let btn1 = document.querySelector('#button');
 let getdata = async()=>{
     console.log(`Retriving Data`);
     let i = await fetch(url);
-    console.log(i);
+    let data = await i.json();
+    console.log(data[0]);
+    p1.innerHTML = data[0].text;
 }
-getdata();
+// btn1.addEventListener('click',getdata);
+// above is done with async await method 
+// now with promise chain
+
+function pro (){
+    fetch(url)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        p1.innerHTML = data[0].text;   
+    })
+}
+btn1.addEventListener('click',pro);
