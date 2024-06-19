d2a10d2e399978e0206db0dc3a5c7c26d6ef8f8c
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-19 10:54:23 +0530

    Update script.js

--- CH13/script.js
+++ CH13/script.js
@@ -1,5 +1,10 @@
 // Dont mind the project ;-;
 
 const url ='https://api.waifu.im/search';
-let promise = fetch(url);
-console.log(promise);
+
+let getdata = async()=>{
+    console.log(`Retriving Data`);
+    let i = await fetch(url);
+    console.log(i);
+}
+getdata();
