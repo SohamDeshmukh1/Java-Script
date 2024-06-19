98165643ba4a35b0627ca3ba8ab9d4b11ecb45f6
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-19 14:37:13 +0530

    promise chains working

--- CH13/script.js
+++ CH13/script.js
@@ -14,19 +14,25 @@ let getdata = async()=>{
     API response. */
     p1.innerHTML = data[0].text;
 }
-// btn1.addEventListener('click',getdata);
+btn1.addEventListener('click',getdata);
 // above is done with async await method 
 // now with promise chain
 
-// function pro (){
-//     fetch(url)
-//     .then((res)=>{
-//         return res.json();
-//     })
-//     .then((data)=>{
-//         p1.innerHTML = data[0].text;   
-//     })
-// }
+function pro (){
+    fetch(url)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        p1.innerHTML = data[0].text;   
+    })
+}
 // btn1.addEventListener('click',pro);
 
 // this is too much confusing so i will prefer async and awit method only
+// Update my own explanantion 
+// fetch(url) give a output as a promise , we use then method to check that promise first and created an obj res
+// if the promise is fullfiled the data we recived is given to res and then parsed the info in human redable info
+// if the return json is sucessfull it creates an another promise and then if its fulllied it goes to the next .then
+// and make its possible to read data
+// I will prefer async functions to do this work
