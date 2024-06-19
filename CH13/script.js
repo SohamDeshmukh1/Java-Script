5a99c1fbf307cdd44c9048b28a4cddb8777085db
sohamdeshmukh1 <spdpvd3515@gmail.com>
2024-06-19 12:16:29 +0530

    Some more Confusing stuff about promise chains

--- CH13/script.js
+++ CH13/script.js
@@ -9,19 +9,24 @@ let getdata = async()=>{
     let i = await fetch(url);
     let data = await i.json();
     console.log(data[0]);
+    /* `p1.innerHTML` is setting the HTML content inside the element selected by the variable `p1`. In
+    this case, it is setting the HTML content to be the text of the first fact retrieved from the
+    API response. */
     p1.innerHTML = data[0].text;
 }
 // btn1.addEventListener('click',getdata);
 // above is done with async await method 
 // now with promise chain
 
-function pro (){
-    fetch(url)
-    .then((res)=>{
-        return res.json();
-    })
-    .then((data)=>{
-        p1.innerHTML = data[0].text;   
-    })
-}
-btn1.addEventListener('click',pro);
+// function pro (){
+//     fetch(url)
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data)=>{
+//         p1.innerHTML = data[0].text;   
+//     })
+// }
+// btn1.addEventListener('click',pro);
+
+// this is too much confusing so i will prefer async and awit method only
